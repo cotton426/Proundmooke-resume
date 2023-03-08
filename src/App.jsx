c37badc956f6d97080b6a1866615ad67b6329679
Proundmooke <fai27sitarak@gmail.com>
2023-03-08 15:43:24 +0700

    all Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Navbar } from "./assets/Navbar";
+import { Title } from "./assets/Title";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/Title.jsx
@@ -0,0 +1,80 @@
+import { Stack, Box, Text, Button, Image } from "@chakra-ui/react";
+
+export function Title() {
+  return (
+    <Stack
+      width="100vw"
+      justify="flex-start"
+      align="flex-start"
+      spacing="10px"
+      height="2321px"
+      background="#F0EEED"
+    >
+      <Stack
+        width="100vw"
+        height="100vh"
+        direction="row"
+        justify="flex-start"
+        align="center"
+        spacing="10%"
+      >
+        <Stack
+          width="60%"
+          justify="flex-start"
+          align="flex-start"
+          spacing="40px"
+          direction="column"
+          paddingLeft="5%"
+        >
+          <Box>
+            <Text
+              fontFamily="Inter"
+              lineHeight="1.26"
+              fontWeight="medium"
+              fontSize="9rem"
+              letterSpacing="-0.04em"
+              color="black"
+              width="100%"
+              height="50%"
+              maxWidth="100%"
+            >
+              I’m Proundmooke. a Front-end developer.
+            </Text>
+          </Box>
+
+          <Text
+            fontFamily="Inter"
+            lineHeight="1.51"
+            fontWeight="regular"
+            fontSize="21px"
+            color="#000000"
+            width="650px"
+            maxWidth="100%"
+          >
+            Front-end developer based in Thailand. I am coding with a clean and
+            beautiful problem solving in mind.
+          </Text>
+
+          <Box>
+            <Box />
+            <Box />
+            <Box />
+            <Box />
+          </Box>
+
+          <Button>Download CV</Button>
+        </Stack>
+
+        <Image
+          alt="proundmooke-picture"
+          src="https://scontent.fkkc1-1.fna.fbcdn.net/v/t39.30808-6/270125614_4795081927205404_3116836941428922850_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeEjuh9koJ3IBUZ1UVCQ9oauknXRutYPe3WSddG61g97dTeU8iQNScqyfPzn-lDK8soyfajnnJQYhWEt9aqaBvC4&_nc_ohc=cz9qSTk1uuIAX-TfHVj&_nc_ht=scontent.fkkc1-1.fna&oh=00_AfDU6_04T683zRty2fdX7YeaFWJDS5HeEARH1MllugCPhA&oe=640DFEAA"
+          width="40%"
+          padding="5%"
+          height="70vh"
+          objectFit="cover"
+          filter="brightness(110%)"
+        />
+      </Stack>
+    </Stack>
+  );
+}
